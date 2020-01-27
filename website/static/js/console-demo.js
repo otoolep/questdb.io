6b7d5d0092c24cfbc9c027afde83379565370cf4
Tanc <tanc@questdb.io>
2020-01-27 18:04:21 +0000

    docs: improving front page

--- website/static/js/console-demo.js
+++ website/static/js/console-demo.js
@@ -4,19 +4,3 @@ window.addEventListener('scroll', function() {
     document.getElementById()
 
 });
-
-window.addEventListener('load', function() {
-
-    document.getElementById('consolebutton').addEventListener('click', myFunction);
-
-    function myFunction() {
-        if(document.getElementById('consolecontainer').style.display=='none') {
-            document.getElementById('consolecontainer').style.display='block';
-            document.getElementById('consolebutton').textContent='Hide Console';
-        } else {
-            document.getElementById('consolecontainer').style.display='none';
-            document.getElementById('consolebutton').textContent='Show Console';
-        }
-    }
-
-});
