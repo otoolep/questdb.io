f3c72f93c286700a72a3c512353e683b974eba6c
Tanc <tanc@questdb.io>
2020-02-27 14:15:58 +0000

    site: deleting unused images

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -88,8 +88,8 @@ const siteConfig = {
     cleanUrl: true,
 
     // Open Graph and Twitter card images.
-    ogImage: 'img/undraw_online.svg',
-    twitterImage: 'img/undraw_tweetstorm.svg',
+    ogImage: 'img/favicon.png',
+    twitterImage: 'img/favicon.png',
 
     // For sites with a sizable amount of content, set collapsible to true.
     // Expand/collapse the links and subcategories under categories.
