dde9d9d831ca3d41b9f56168996d16f50783b437
Vlad Ilyushchenko <bluestreak@gmail.com>
2020-03-06 20:38:33 +0000

    fixed: careers responsiveness

--- website/pages/en/careers.js
+++ website/pages/en/careers.js
@@ -66,7 +66,7 @@ class Index extends React.Component {
                         Want to join an ambitious team whose mission to push the boundaries of database
                         performance? <b> We're hiring!</b> QuestDB is based in <b>London</b> and <b>San Francisco</b>,
                         and backed by
-                        leading venture capital firms.
+                        leading venture capital funds.
                     </p>
 
                     <img className="careers-img" src='/img/team.svg'/>
