48a30291966c365d4246d9754c4f97933a2c586f
Vlad Ilyushchenko <bluestreak@gmail.com>
2020-03-15 02:36:18 +0000

    chore: front page restyle

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -223,24 +223,9 @@ const siteConfig = {
     /* Colors for website */
     /*    Both set to transparent to customise manually*/
     colors: {
-        primaryColor: '#2371b5',
-        secondaryColor: '#2371b5',
+        // primaryColor: '#2371b5',
+        // secondaryColor: '#2371b5',
     },
-
-    /* Custom fonts for website */
-    /*
-    fonts: {
-      myFont: [
-        "Times New Roman",
-        "Serif"
-      ],
-      myOtherFont: [
-        "-apple-system",
-        "system-ui"
-      ]
-    },
-
-    */
 
     // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
     copyright: `Copyright © ${new Date().getFullYear()} QuestDB`,
